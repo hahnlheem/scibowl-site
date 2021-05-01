083aa85642c5f20d4861cae9a784f3c9145a63ab
Alvin Chen <alvinchen3825@gmail.com>
2021-05-01 18:08:38 -0400

    add clock

--- client/src/components/modules/Clock.js
+++ client/src/components/modules/Clock.js
@@ -27,8 +27,8 @@ class Clock extends Component {
     render() {
       return (
         <div>
-          <h1>Hello, world!</h1>
-          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+          <h1>YEET HAW</h1>
+          <h2>THE TIME IS CURRENTLY {this.state.date.toLocaleTimeString()}.</h2>
         </div>
       );
     }
