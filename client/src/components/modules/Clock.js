fb1fa973a966a8df072538bb759f12ac18bded70
Hahn Lheem <hahnlheem@gmail.com>
2021-05-07 17:41:50 -0400

    countdown timer as input

--- client/src/components/modules/Clock.js
+++ client/src/components/modules/Clock.js
@@ -4,7 +4,7 @@ import { Link } from "@reach/router";
 class Clock extends Component {
     constructor(props) {
       super(props);
-      this.state = { n: 20 };
+      this.state = { timeLeft: 20 };
       this.tick = this.tick.bind(this);
     }
   
@@ -18,22 +18,30 @@ class Clock extends Component {
 
   
     tick() {
-        this.state.n = 20;
         this.interval = setInterval(() => {
-            if (this.state.n>0) {
+            if (this.state.timeLeft>0) {
                 this.setState(prevState => ({
-                    n: prevState.n-1
+                    timeLeft: prevState.timeLeft-1
                 }));
             }
             
         }, 1000);
     }
+
+    setSeconds = (e) => {
+      this.setState({
+        timeLeft: e.target.value
+      });
+    };
   
     render() {
       return (
         <div className="full-page">
           <h1>random text</h1>
-          <h2>Countdown: {this.state.n}.</h2>
+          <h2>Countdown:</h2>
+          <div>
+            <input type="text" onChange={ this.setSeconds } value={ this.state.timeLeft } />
+          </div>
           <button onClick={this.tick} type="button" class="btn btn-primary">Start Bonus</button>
           <button onClick={this.decreaseBy1} type="button" class="btn btn-danger">Stop Bonus</button>
         </div>
