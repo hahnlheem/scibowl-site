65b3eb77cb4ebab4e48a7676d65473664f51c56d
Hahn Lheem <hahnlheem@gmail.com>
2021-05-10 18:47:49 -0400

    added half timer

--- client/src/components/modules/Clock.js
+++ client/src/components/modules/Clock.js
@@ -7,11 +7,13 @@ class Clock extends Component {
     constructor(props) {
       super(props);
       this.state = { 
-        timeLeft: 0,
-        isPaused: true,
-        isStartPressed: false,
+        questionTime: 0,
+        halfTime: 480,
+        isHPaused: true,
+        isQPaused: true,
+        isHStartPressed: false,
+        isQStartPressed: false,
       };
-      this.tick = this.tick.bind(this);
     }
   
     // componentDidMount() {
@@ -21,66 +23,129 @@ class Clock extends Component {
     // componentWillUnmount() {
     //   clearInterval(this.interval);
     // }
-
-  
-    tick() {
+    tickH = () => {
       this.interval = setInterval(() => {
-          if (this.state.timeLeft > 0 && !(this.state.isPaused)) {
+          if (this.state.halfTime > 0 && !(this.state.isHPaused)) {
               this.setState(prevState => ({
-                  timeLeft: parseInt(prevState.timeLeft)-1
+                  halfTime: parseInt(prevState.halfTime)-1
+              }));
+          }
+          
+      }, 1000);
+    }
+    //currently it seems like tickH and tickQ use same interval? not that big of deal but,,,
+    tickQ = () => {
+      this.interval = setInterval(() => {
+          if (this.state.questionTime > 0 && !(this.state.isQPaused)) {
+              this.setState(prevState => ({
+                  questionTime: parseInt(prevState.questionTime)-1
               }));
           }
           
       }, 1000);
     }
 
-    negPause = (e) => {
+    negQPause = (e) => {
       this.setState(prevState => ({
-        isPaused: !(this.state.isPaused)
+        isQPaused: !(this.state.isQPaused)
+      }));
+    }
+
+    negHPause = (e) => {
+      this.setState(prevState => ({
+        isHPaused: !(this.state.isHPaused)
       }));
     }
 
     startBonus = (e) => {
       this.setState({
-        isPaused: false,
-        timeLeft: 20
+        isQPaused: false,
+        questionTime: 20
       });
-      if (!this.state.isStartPressed) {
-        this.tick();
+      if (!this.state.isQStartPressed) {
+        this.tickQ();
       }
       this.setState({
-        isStartPressed: true
+        isQStartPressed: true
       });
     }
 
     startTossup = (e) => {
       this.setState({
-        isPaused: false,
-        timeLeft: 5
+        isQPaused: false,
+        questionTime: 5
       });
-      if (!this.state.isStartPressed) {
-        this.tick();
+      if (!this.state.isQStartPressed) {
+        this.tickQ();
       }
       this.setState({
-        isStartPressed: true
+        isQStartPressed: true
       });
     }
 
-    setSeconds = (e) => {
+    startHalf = (e) => {
       this.setState({
-        timeLeft: e.target.value
+        isHPaused: false,
+        halfTime: 480
       });
-    };
+      if (!this.state.isHStartPressed) {
+        this.tickH();
+      }
+      this.setState({
+        isHStartPressed: true
+      });
+    }
+
+    resetHalf = (e) => {
+      this.setState({
+        isHPaused: true,
+        halfTime: 480, // replace this with props once we get game settings going
+      });
+    }
+
+    setHalfTime = (e) => { // split by :
+      this.setState({
+        halfTime: e.target.value
+      });
+    }
+
+    setQSeconds = (e) => {
+      this.setState({
+        questionTime: e.target.value
+      });
+    }
   
     render() {
+      let halfTimeSecond = (this.state.halfTime % 60 > 9) ? (this.state.halfTime % 60) : ("0" + this.state.halfTime % 60);
       return (
         <div className="full-page">
           <h1>random text</h1>
           <div className="float-container">
-            <h2>Countdown: <input type="number" onChange={ this.setSeconds } value={ this.state.timeLeft } className="questionClock"/></h2>
-            <button onClick={this.startTossup} type="button" class="btn btn-primary">Start Tossup</button>
-            <button onClick={this.startBonus} type="button" class="btn btn-danger">Start Bonus</button>
-            <button onClick={this.negPause} type="button" class="btn btn-warning">Pause/Play</button>
+            {/* make these float childs a bit more flexible */}
+            <div className="float-child">
+              <h2>Half Timer: 
+                <input type="text" 
+                  onChange={ this.setHalfTime } 
+                  value={ Math.floor(this.state.halfTime/60) + ":" + halfTimeSecond } 
+                  className="halfClock"
+                />
+              </h2>
+              <button onClick={this.startHalf} type="button" class="btn btn-primary">Start Half</button>
+              <button onClick={this.negHPause} type="button" class="btn btn-warning">Pause/Play</button>
+              <button onClick={this.resetHalf} type="button" class="btn btn-danger">Reset</button>
+            </div>
+            <div className="float-child">
+              <h2>Question Timer: 
+                <input type="number" 
+                  onChange={ this.setQSeconds } 
+                  value={ this.state.questionTime } 
+                  className="questionClock"
+                />
+              </h2>
+              <button onClick={this.startTossup} type="button" class="btn btn-primary">Start Tossup</button>
+              <button onClick={this.startBonus} type="button" class="btn btn-danger">Start Bonus</button>
+              <button onClick={this.negQPause} type="button" class="btn btn-warning">Pause/Play</button>
+            </div>
           </div>
         </div>
       );
